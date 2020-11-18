979b5aa5cf48d4f9dad1906c6b6aa3732bcdddbb
elijahsamuels <elijahsamuels@gmail.com>
2020-11-17 17:03:47 -0700

    commit tree 048051e612f889b47b95d33d81f645edd683a2c8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,46 @@
+require "pry"
+
 # Your code goes here!
+
+# listen = Anagram.new("listen")
+# listen.match(%w(enlists google inlets banana))
+
+
+
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(words)
+        words.select do |a|
+            a.split("").sort == word.split("").sort
+        end
+    end
+end
+
+
+
+
+
+    # def match
+
+    #     @word.each do |char|
+    #         char == char
+    #     end
+    # end
+
+
+        # if 1 < 2
+
+        #     puts "this works"
+        #     # return []   
+  
+        # end
+    
+
+    # if word.chars.sort.join =~ word.chars.sort.join
+
